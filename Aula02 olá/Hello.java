15db99955b44f003d64b89cb7202f7073d77539f
Ricardo Felipe Benjamin <ricardofelipebenjamin2003@gmail.com>
2025-02-28 20:37:02 -0300

    primeiro commit

---
+++ Aula02 olá/Hello.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Hello {
+    
+public static void main(String[] args) {
+
+     Scanner scan =  new Scanner(System.in);
+     int  option = 0;
+
+
+     while(option != 4){ 
+        System.out.println("Escolha uma Opção: ");
+        System.out.println("[1]Imprimir Hello World");
+        System.out.println("[4]Sair");
+        option = scan.nextInt();
+
+        switch(option){
+
+            case 1:
+                System.out.println("Hello World: ");
+            break;
+            
+            case 4:
+                System.out.println("Saindo...");
+            break;
+
+            default:
+            System.out.println("Invalido tente novamente: ");
+
+        }
+
+
+
+
+        }
+ 
+ 
+ 
+    }
+}
